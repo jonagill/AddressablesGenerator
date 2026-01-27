2a434eab065f6be6847c57ef084b85f0d771810a
Jon Gill <jonagill@gmail.com>
2026-01-27 10:35:00 -0800

    Spread out our AddressabelsGeneratorCallbacksOrders so that users can insert their own code between steps if necessary.

--- Packages/com.jonagill.addressablesgenerator/Editor/AddressablesGeneratorCallbackOrder.cs
+++ Packages/com.jonagill.addressablesgenerator/Editor/AddressablesGeneratorCallbackOrder.cs
@@ -3,9 +3,9 @@ namespace AddressablesGenerator
     public enum AddressablesGeneratorCallbackOrder
     {
         PreBuildCleanup = -99, // AddressablesCleanupBuildProcessor.cs
-        GenerateGroups = -2, // AddressablesGeneratorBuildProcessor.cs
-        SplitGroups = -1, // AddressablesGroupSplitterBuildProcessor.cs
-        GenerateDependencyGroups = 0, // AddressablesDependencyBuildProcessor.cs 
+        GenerateGroups = -30, // AddressablesGeneratorBuildProcessor.cs
+        SplitGroups = -20, // AddressablesGroupSplitterBuildProcessor.cs
+        GenerateDependencyGroups = -10, // AddressablesDependencyBuildProcessor.cs 
         BuildAddressables = 1, // AddressablesPlayerBuildProcessor.cs (built-in)
     }
 }
